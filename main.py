5346c0be9f638abe3594a8b19296b33d3d2b5a52
Alvin Wan <alvinwan1@gmail.com>
2020-01-30 01:22:37 -0800

    added lr adjustment + cifar10node dataset

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ from utils.utils import progress_bar
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--dataset', choices=('cifar10', 'cifar10node'))
+parser.add_argument('--wnid', help='wordnet id for cifar10node dataset',
+                    default='fall11')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 parser.add_argument('--test', action='store_true', help='run dataset tests')
@@ -66,10 +69,14 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+if args.dataset == 'cifar10':
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+elif args.dataset == 'cifar10node':
+    trainset = CIFAR10NodeDataset(args.wnid, root='./data', train=True, download=True, transform=transform_train)
+    testset = CIFAR10NodeDataset(args.wnid, root='./data', train=False, download=True, transform=transform_test)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
@@ -78,7 +85,7 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship'
 print('==> Building model..')
 # net = VGG('VGG19')
 # net = ResNet18()
-# net = PreActResNet18()
+net = PreActResNet18()
 # net = GoogLeNet()
 # net = DenseNet121()
 # net = ResNeXt29_2x64d()
@@ -88,7 +95,7 @@ print('==> Building model..')
 # net = ShuffleNetG2()
 # net = SENet18()
 # net = ShuffleNetV2(1)
-net = EfficientNetB0()
+# net = EfficientNetB0()
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
@@ -106,8 +113,19 @@ if args.resume:
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 
+def adjust_learning_rate(epoch, lr):
+    if epoch <= 150:  # 32k iterations
+      return lr
+    elif epoch <= 250:  # 48k iterations
+      return lr/10
+    else:
+      return lr/100
+
 # Training
 def train(epoch):
+    lr = adjust_learning_rate(epoch, args.lr)
+    optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=5e-4)
+
     print('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
