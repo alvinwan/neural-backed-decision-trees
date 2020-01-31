f1b0a027948cb8589cb5a5571b229b76f52fc779
Alvin Wan <alvinwan1@gmail.com>
2020-01-30 21:14:31 -0800

    support cifar10 and cifar100

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ import models
 from utils import progress_bar
 
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser = argparse.ArgumentParser(description='PyTorch CIFAR Training')
+parser.add_argument('--dataset', default='CIFAR10', choices=('CIFAR10', 'CIFAR100'))
 parser.add_argument('--model', default='EfficientNetB0', choices=list(models.get_model_choices()))
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
@@ -39,10 +40,12 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+dataset = getattr(torchvision.datasets, args.dataset)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+trainset = dataset(root='./data', train=True, download=True, transform=transform_train)
+testset = dataset(root='./data', train=False, download=True, transform=transform_test)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
