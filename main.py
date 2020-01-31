50aaa48175cc74eb80cd58f31d4b32430e555d16
Alvin Wan <alvinwan1@gmail.com>
2020-01-30 21:41:01 -0800

    added batch size support

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from utils import progress_bar
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR Training')
+parser.add_argument('--batch-size', default=128, help='Batch size used for training')
 parser.add_argument('--dataset', default='CIFAR10', choices=('CIFAR10', 'CIFAR100'))
 parser.add_argument('--model', default='ResNet18', choices=list(models.get_model_choices()))
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
@@ -45,7 +46,7 @@ dataset = getattr(torchvision.datasets, args.dataset)
 trainset = dataset(root='./data', train=True, download=True, transform=transform_train)
 testset = dataset(root='./data', train=False, download=True, transform=transform_test)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
