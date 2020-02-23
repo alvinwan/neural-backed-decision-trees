27809694219f61115e3e15f9c6fd2da61fee9b33
Alvin Wan <alvinwan1@gmail.com>
2020-02-22 17:40:39 -0800

    gg 🥵

--- scripts/generate_graphs.sh
+++ scripts/generate_graphs.sh
@@ -1,6 +1,5 @@
 for dataset in CIFAR10 CIFAR100 TinyImagenet200;
   do
-    do
-      python generate_graph.py --dataset=${dataset};
-      python test_generated_graph.py --dataset=${dataset};
-    done;
+    python generate_graph.py --dataset=${dataset};
+    python test_generated_graph.py --dataset=${dataset};
+  done;
