f11ff6b4a0a08387dbf3c91cea05b73bcae38008
Alvin Wan <alvinwan1@gmail.com>
2020-02-23 03:15:56 -0800

    generate single path trees

--- scripts/generate_graphs.sh
+++ scripts/generate_graphs.sh
@@ -1,5 +1,7 @@
 for dataset in CIFAR10 CIFAR100 TinyImagenet200;
 do
+  python generate_graph.py --dataset=${dataset} --single-path;
+  python test_generated_graph.py --dataset=${dataset} --single-path;
   for extra in 0 20 50 100;
   do
     python generate_graph.py --dataset=${dataset} --extra=${extra};
