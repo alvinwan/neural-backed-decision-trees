c2fe1f64a6d696b57308d8773084ac0e4509db96
Daniel Ho <danielho@berkeley.edu>
2020-02-25 20:29:41 -0800

    fixed typo

--- utils/data/imagenet.py
+++ utils/data/imagenet.py
@@ -104,7 +104,7 @@ class Imagenet1000(Dataset):
     """ImageNet dataloader"""
 
     transform_train = transforms.Compose([
-        transforms.RandomSizedCrop(224),
+        transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
