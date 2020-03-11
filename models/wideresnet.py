7c4ea266500495eaa7e998943df1747d4cab9c85
Alvin Wan <alvinwan1@gmail.com>
2020-03-10 21:29:38 -0700

    usin cifar UGH

--- models/wideresnet.py
+++ models/wideresnet.py
@@ -1,4 +1,4 @@
-from pytorchcv.models.wrn import get_wrn
+from pytorchcv.models.wrn_cifar import get_wrn_cifar
 import torch.nn as nn
 
 __all__ = ('wrn28_10',)
@@ -11,6 +11,6 @@ def wrn28_10(num_classes=10, **kwargs):
     final pooled feature map is 16x16 instead of 8x8. So needs another stride 2
     stage, technically.
     """
-    net = get_wrn(num_classes=num_classes, blocks=28, width_factor=10, model_name="wrn28_10", **kwargs)
+    net = get_wrn_cifar(num_classes=num_classes, blocks=28, width_factor=10, model_name="wrn28_10", **kwargs)
     net.features.final_pool = nn.AdaptiveAvgPool2d((1, 1))
     return net
