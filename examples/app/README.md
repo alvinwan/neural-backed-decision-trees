38b2812df966a4fcd1d9539a5e330810aa2c348f
Alvin Wan <alvinwan1@gmail.com>
2020-03-25 15:45:51 -0700

    example app

---
+++ examples/app/README.md
@@ -0,0 +1,3 @@
+super simple flask app for serving NBDT predictions
+
+deployed to repl.it (link tbd)
---
+++ examples/app/app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from nbdt.model import SoftNBDT
+from nbdt.models import wrn28_10_cifar10
+from torchvision import transforms
+from nbdt.utils import DATASET_TO_CLASSES, load_image_from_path, maybe_install_wordnet
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    # load pretrained NBDT
+    model = wrn28_10_cifar10()
+    model = SoftNBDT(
+      pretrained=True,
+      dataset='CIFAR10',
+      arch='wrn28_10_cifar10',
+      hierarchy='wordnet',
+      model=model)
+
+    # load + transform image
+    im = load_image_from_path("https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=32")
+    transform = transforms.Compose([
+      transforms.Resize(32),
+      transforms.CenterCrop(32),
+      transforms.ToTensor(),
+      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    x = transform(im)[None]
+
+    # run inference
+    outputs, decisions = model.forward_with_decisions(x)  # use `model(x)` to obtain just logits
+    _, predicted = outputs.max(1)
+    return {
+        'predicted': [DATASET_TO_CLASSES['CIFAR10'][pred] for pred in predicted],
+        'decisions': [[info['name'] for info in decision] for decision in decisions]
+    }
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
