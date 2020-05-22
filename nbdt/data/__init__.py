73ad2414702c636dca3ef795807e9d1192e8feaf
Daniel Ho <danielho@berkeley.edu>
2020-05-21 17:25:19 -0700

    added ade20k support

--- nbdt/data/__init__.py
+++ nbdt/data/__init__.py
@@ -2,4 +2,5 @@ from .custom import *
 from .imagenet import *
 from .pascal_context import *
 from .lip import *
+from .ade20k import *
 from torchvision.datasets import *
--- nbdt/utils.py
+++ nbdt/utils.py
@@ -20,7 +20,7 @@ import nltk
 # tree-generation consntants
 METHODS = ('wordnet', 'random', 'induced')
 DATASETS = ('CIFAR10', 'CIFAR100', 'TinyImagenet200', 'Imagenet1000',
-    'Cityscapes', 'PascalContext', 'LookIntoPerson')
+    'Cityscapes', 'PascalContext', 'LookIntoPerson', 'ADE20K')
 DATASET_TO_NUM_CLASSES = {
     'CIFAR10': 10,
     'CIFAR100': 100,
@@ -29,6 +29,7 @@ DATASET_TO_NUM_CLASSES = {
     'Cityscapes': 19,
     'PascalContext': 59,
     'LookIntoPerson': 20,
+    'ADE20K': 150,
 }
 DATASET_TO_CLASSES = {
     'CIFAR10': [
