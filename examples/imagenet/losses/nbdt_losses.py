6bfca314e413d733ff9add00a6dfedaa8bca5846
Alvin Wan <alvinwan1@gmail.com>
2021-01-25 01:37:15 -0800

    added weight config

--- examples/imagenet/losses/nbdt_losses.py
+++ examples/imagenet/losses/nbdt_losses.py
@@ -5,15 +5,17 @@ from nbdt.loss import SoftTreeSupLoss
 
 @register_loss("NBDTTreeSupLoss")
 class NBDTTreeSupLoss(SoftTreeSupLoss, ClassyLoss):
-    def __init__(self):
+    def __init__(self, tree_supervision_weight=5):
         super().__init__(
             criterion=nn.CrossEntropyLoss().cuda(),
             dataset='Imagenet1000',
-            tree_supervision_weight=10,
+            tree_supervision_weight=tree_supervision_weight,
             hierarchy='induced-efficientnet_b7b'
         )
 
     @classmethod
     def from_config(cls, config):
         # We don't need anything from the config
-        return cls()
+        return cls(
+            tree_supervision_weight=config.get("tree_supervision_weight", 5)
+        )
