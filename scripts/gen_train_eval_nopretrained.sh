2e76a854d1a3de382c2532ba6a6cb9037b1d77ab
MDutro <mdutrolaw@gmail.com>
2021-03-19 10:11:29 -0500

    Change '--model' flag to '--arch' in bash scripts

--- scripts/gen_train_eval_nopretrained.sh
+++ scripts/gen_train_eval_nopretrained.sh
@@ -6,16 +6,16 @@ dataset=CIFAR10
 weight=1
 
 # 0. train the baseline neural network
-python main.py --dataset=${dataset} --model=${model}
+python main.py --dataset=${dataset} --arch=${model}
 
 # 1. generate hieararchy -- for models without a pretrained checkpoint, use `checkpoint`
 nbdt-hierarchy --dataset=${dataset} --checkpoint=./checkpoint/ckpt-${dataset}-${model}.pth
 
 # 2. train with soft tree supervision loss -- for models without a pretrained checkpoint, use `path-resume` OR just train from scratch, without `path-resume`
 # python main.py --lr=0.01 --dataset=${dataset} --model=${model} --hierarchy=induced-${model} --path-resume=./checkpoint/ckpt-${dataset}-${model}.pth --loss=SoftTreeSupLoss --tree-supervision-weight=${weight}  # fine-tuning
-python main.py --dataset=${dataset} --model=${model} --hierarchy=induced-${model} --loss=SoftTreeSupLoss --tree-supervision-weight=${weight}  # training from scratch
+python main.py --dataset=${dataset} --arch=${model} --hierarchy=induced-${model} --loss=SoftTreeSupLoss --tree-supervision-weight=${weight}  # training from scratch
 
 # 3. evaluate with soft then hard inference
 for analysis in SoftEmbeddedDecisionRules HardEmbeddedDecisionRules; do
-  python main.py --dataset=${dataset} --model=${model} --hierarchy=induced-${model} --loss=SoftTreeSupLoss --eval --resume --analysis=${analysis} --tree-supervision-weight=${weight}
+  python main.py --dataset=${dataset} --arch=${model} --hierarchy=induced-${model} --loss=SoftTreeSupLoss --eval --resume --analysis=${analysis} --tree-supervision-weight=${weight}
 done
--- scripts/gen_train_eval_resnet.sh
+++ scripts/gen_train_eval_resnet.sh
@@ -7,10 +7,10 @@ for i in "CIFAR10 1" "CIFAR100 1" "TinyImagenet200 10"; do
   nbdt-hierarchy --dataset=${dataset} --arch=ResNet18
 
   # 2. train with soft tree supervision loss
-  python main.py --dataset=${dataset} --model=${model} --hierarchy=induced-${model} --loss=SoftTreeSupLoss --tree-supervision-weight=${weight}
+  python main.py --dataset=${dataset} --arch=${model} --hierarchy=induced-${model} --loss=SoftTreeSupLoss --tree-supervision-weight=${weight}
 
   # 3. evaluate with soft then hard inference
   for analysis in SoftEmbeddedDecisionRules HardEmbeddedDecisionRules; do
-    python main.py --dataset=${dataset} --model=${model} --hierarchy=induced-${model} --loss=SoftTreeSupLoss --tree-supervision-weight=${weight} --eval --resume --analysis=${analysis}
+    python main.py --dataset=${dataset} --arch=${model} --hierarchy=induced-${model} --loss=SoftTreeSupLoss --tree-supervision-weight=${weight} --eval --resume --analysis=${analysis}
   done
 done;
